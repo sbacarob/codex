9e88adb0bddeab361568dc434fd9ef43c35f9fa1
Santiago Bacaro <sbacarob@outlook.com>
2020-02-18 20:09:04 +0100

    add general purpose signed request function

--- test/codex_test.exs
+++ test/codex_test.exs
@@ -1,4 +1,19 @@
 defmodule CodexTest do
   use ExUnit.Case
-  doctest Codex
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  setup_all do
+    HTTPoison.start
+  end
+
+  test "signed request returns the expected result" do
+    ExVCR.Config.filter_request_headers("Authorization")
+    ExVCR.Config.filter_sensitive_data("<key>.*<\/key>", "<key>[CDATA[YOUR_API_KEY]]</key>")
+    use_cassette "signed_request" do
+      assert {:ok, xml_data} = Codex.signed_request("api/auth_user", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+      assert xml_data =~ "<authentication>true</authentication>"
+      assert xml_data =~ "<name>Santiago Bacaro</name>"
+    end
+  end
 end
